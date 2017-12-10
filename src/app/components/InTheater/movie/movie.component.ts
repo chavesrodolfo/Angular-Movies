7046bce9ae47f48dcff54dfb17038221865cac99
Rodolfo <chavesrodolfo@gmail.com>
2017-12-10 01:17:06 -0200

    adjustments

--- src/app/components/InTheater/movie/movie.component.ts
+++ src/app/components/InTheater/movie/movie.component.ts
@@ -57,7 +57,7 @@ export class MovieComponent implements OnInit {
 
       this._moviesService.getMovieCredits(id).subscribe( res => {
         res.cast = res.cast.filter( item => { return item.profile_path });
-        this.cast = res.cast.slice(0, 8);
+        this.cast = res.cast.slice(0, 4);
       });
 
       this._moviesService.getMovieVideos(id).subscribe( res => {
